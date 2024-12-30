e2d50e2e5b8df00c5cd1a04f8512c14b8cf65864
Sajadev404 <empresasarn@gmail.com>
2024-12-30 19:42:58 +0000

    tests

---
+++ src/api/test.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    #PRUEBA DE GIT PARA VER COMO VA git push origin
+         user = User()
+            user.email = "test_user" + str(x) + "@test.com"
+            user.password = "123456"
+             user = User()
+            user.email = "test_user" + str(x) + "@test.com"
+            user.password = "123456"
+             user = User()
+            user.email = "test_user" + str(x) + "@test.com"
+            user.password = "123456"
+             user = User()
+            user.email = "test_user" + str(x) + "@test.com"
+            user.password = "123456"
+            
