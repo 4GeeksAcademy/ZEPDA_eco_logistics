6b3241e7b2d6193f007266294ecd70a297f2c6d7
Sajadev404 <empresasarn@gmail.com>
2024-12-30 18:51:57 +0000

    cambio footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,6 +5,7 @@ export const Footer = () => (
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			<h1>cambios en footer</h1>
 		</p>
 	</footer>
 );
