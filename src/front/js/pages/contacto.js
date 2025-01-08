94ae028ad55ea0c0786bce2906bf8c31a4c74385
Sajadev404 <empresasarn@gmail.com>
2025-01-08 11:03:36 +0000

    test

--- src/front/js/pages/contacto.js
+++ src/front/js/pages/contacto.js
@@ -1,17 +1,35 @@
-import React, { useState, useEffect, useContext } from "react";
-import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import imagen from "../../img/zepda-web-img/Driving-Green-Email-Marketing-for-Eco-Friendly-Products-scaled-1-2.jpg";
 
-export const Contacto = props => {
-const { store, actions } = useContext(Context);
-    const params = useParams();
+
+export const Contacto = () => {
+    const { store, actions } = useContext(Context);
+    
 
     return (
-    <div>
-        <h1>empresas</h1>
-    </div>
-    )
 
-};
+
+        <> 
+     
+        
+        <div className="container mt-4">
+            <h1>Contacto</h1>
+            <div className="contacto-img-container">
+                <img src={imagen} className="contacto-img" alt="contacto" />
+            </div>
+        
+        <p className="mt-5">Tienes alguna duda o necesitas información sobre nuestros servicios? Escríbenos sin compromiso y te ayudaremos a resolver tus dudas.</p>
+            <div className="mb-3 col-md-6">
+                <label for="exampleFormControlInput1" className="form-label">Email</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+            <div className="mb-3 col-md-6">
+                <label for="exampleFormControlTextarea1" className="form-label">Mensaje</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <button className="btn mt-2">Enviar</button>
+            </div>
+            </div>
+        </>
+    )
+}
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,11 +2,65 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { JumbotronCarousel } from "../component/jumbocarousel";
 import "../../styles/home.css";
+import { NewsSection } from "../component/NewsSection";
+import { Link } from "react-router-dom";
+import { importAllImages } from "../../utils/importAllImages";
+
+const logos = importAllImages(require.context("../../img/logos", false, /\.(png|jpe?g|svg|webp)$/));
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
+
+		<>
+		
 		<JumbotronCarousel/>
+		<div className="container mt-4">
+			<h1> 
+				El cambio ya empezó, el momento de sumarse es ahora!
+			</h1>
+			<p>
+			Trabajamos para disminuir el impacto ambiental y generar acciones positivas para el planeta y la sociedad. 
+			Te ofrecemos las mejores soluciones eco-friendly para tu empresa. El cambio ya empezó, el momento de sumarse es ahora!
+			</p>
+			<p>
+			Como funciona <strong>Zepda Eco-logistics</strong>?
+			</p>
+			<p>
+			En esta plataforma podrás encontrar soluciones sostenibles para que tu empresa sea más eco-friendly. 
+			Podrás encontrar servicios que te ayudarán a disminuir el impacto ambiental de tu empresa. Además, 
+			podrás conocer el impacto positivo que generas al adquirir estos productos y servicios.
+			Sólo tienes que crear una cuenta en nuestra plataforma y empezar a disfrutar de los beneficios de ser eco-friendly.
+			</p>
+			<button className="btn">súmate al cambio</button>
+
+			
+
+			<div className="mt-4">
+
+				<h1>Empresas colaboradoras</h1>
+			<p>Nuestra plataforma ofrece servicios para empresas que necesitan encontrar soluciones ecológicas y sostenibles 
+				para sus diferentes actividades, principalmente en los aspectos logísticos como el packaging de sus productos, 
+				transportes, materiales, gestión de residuos y/o digitalización de su negocio, centralizando en un solo sitio web 
+				todos los servicios disponibles y haciendo más facil los procesos de búsqueda y contratación. Si quieres formar 
+				parte de nuestro equipo <Link className="links" to={"/"}> <strong>únete a nosotros como empresa colaboradora.</strong></Link></p>	
+
+			</div>
+			<p>Nuestros colaboradores:</p>
+			<div className="logos-container">
+				
+            {logos.map((logo, index) => (
+              <div className="logo-item" key={index}>
+                <img src={logo} alt={`logo-${index}`} className="logo-image" />
+              </div>
+            ))}
+          </div>
+
+		  <NewsSection/>
+
+
+		</div>
+		</>
 	);
 };
